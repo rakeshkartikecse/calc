c995ec4ba1d3d8d28556ae29449a51685344964a
NewApps <rakeshkartikecse@gmail.com>
2021-03-09 13:24:21 +0530

    Calculator in Node.js

---
+++ server.js
@@ -0,0 +1,30 @@
+//jshint esversion:6
+
+const express= require('express');
+const bodyParser=require("body-parser");
+const app=express();
+
+app.use(bodyParser.urlencoded({extends:true}));
+app.get("/", function(request,response)
+{
+    response.sendFile(`${__dirname}/index.html`);
+})
+
+app.post("/",function(req,res)
+{
+    //res.send("Hi");
+    var num1=(Number)(req.body.num1);
+    var num2=(Number)(req.body.num2);
+    var result=num1+num2;
+
+    res.send("The sum of the number is "+ result);
+})
+
+app.get("/calculator", function(req,res)
+{
+    res.send("hello");
+})
+
+app.listen(3000,function(){
+    console.log("Hello world!");
+});
